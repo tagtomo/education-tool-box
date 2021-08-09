76688e077df4e03bfbb8fe7190549e21aa56d9ff
田栗智幸 <taguri@birdworks.co.jp>
2021-08-09 07:10:54 +0000

    canvasを4kサイズへ変更

--- src/components/dashboard/flashcard.tsx
+++ src/components/dashboard/flashcard.tsx
@@ -36,14 +36,18 @@ function shuffleArray(inputArray: any) {
 export default function FlashcardComponent(): JSX.Element {
   const [flashcard, setFlashcard] = React.useState<FlashCard>(initialStateFlashcard);
   const [inData, setInData] = useState(flashcard);
+  const resolution = [
+    { type: "4k", width: 3840, height: 2160 },
+    { type: "FullHD", width: 1920, height: 1080 },
+  ]
 
-  const width = 1920; // FullHD
-  const height = 1080; // FullHD
-  const dispalyWidth = 320; // FullHD
-  const dispalyHeight = 180; // FullHD
+  const width = resolution[0].width;
+  const height = resolution[0].height;
+  const dispalyWidth = 320;
+  const dispalyHeight = 180;
 
   const flashCardCanvasRef = React.useRef<HTMLCanvasElement>(null);
-  const { initRecorder, startRecording, endRecording } = useCanvasRecorder(flashCardCanvasRef);
+  const { recording, initRecorder, startRecording, endRecording } = useCanvasRecorder(flashCardCanvasRef);
 
   const fullScreenElement = React.useRef(null);
   const { open } = useFullScreen(fullScreenElement);
@@ -124,8 +128,12 @@ export default function FlashcardComponent(): JSX.Element {
         )}
         <Button onClick={onFullScreen}>フルスクリーン</Button>
         <input type='file' onChange={onFileInputChange} />
-        <Button onClick={onStartRecoding}>REC</Button>
-        <Button onClick={onEndRecoding}>REC END</Button>
+        {recording ? (
+          <Button onClick={onEndRecoding}>REC END</Button>
+        ) : (
+          <Button onClick={onStartRecoding}>REC START</Button>
+        )}
+
         <div>
           <p>タイトル：{flashcard.title}</p>
           <p>終了テキスト：{flashcard.endText}</p>
